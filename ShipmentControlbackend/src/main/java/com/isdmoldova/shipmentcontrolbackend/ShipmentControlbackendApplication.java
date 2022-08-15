63f994a88b3dcd74ad928fc97f5c6194f53a5ec0
devLeon93 <leonidbarsdev1993@gmail.com>
2022-08-15 12:51:57 +0300

    initialize project

---
+++ ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/ShipmentControlbackendApplication.java
@@ -0,0 +1,13 @@
+package com.isdmoldova.shipmentcontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentControlbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentControlbackendApplication.class, args);
+	}
+
+}
---
+++ ShipmentControlbackend/src/test/java/com/isdmoldova/shipmentcontrolbackend/ShipmentControlbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isdmoldova.shipmentcontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentControlbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
