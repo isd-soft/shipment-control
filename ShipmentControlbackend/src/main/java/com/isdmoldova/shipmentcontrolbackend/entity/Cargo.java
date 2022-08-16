a0ba1437d5af8db03a36c7b736d030280e56529e
michaels1999 <mihaidodi98@gmail.com>
2022-08-16 14:42:12 +0300

    Created entities

---
+++ ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/entity/Cargo.java
@@ -0,0 +1,59 @@
+package com.isdmoldova.shipmentcontrolbackend.entity;
+
+import com.isdmoldova.shipmentcontrolbackend.entity.enums.CargoStatus;
+import com.isdmoldova.shipmentcontrolbackend.entity.enums.CargoType;
+import com.isdmoldova.shipmentcontrolbackend.entity.enums.TransportationType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cargo")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "tracking_number")
+    private String trackingNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Route route;
+
+    @Column(name = "total_volume")
+    private Double totalVolume;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "cargo_type")
+    private CargoType cargoType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transportation_type")
+    private TransportationType transportationType;
+
+    private String destination;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "cargo_status")
+    private CargoStatus cargoStatus;
+
+
+}
---
+++ ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/entity/enums/CargoStatus.java
@@ -0,0 +1,8 @@
+package com.isdmoldova.shipmentcontrolbackend.entity.enums;
+
+public enum CargoStatus {
+
+    ANALYZING ,
+    PREPARING ,
+    DELIVERED
+}
