6496ccba76540f1fed0e9f2f6affd10568771336
devLeon93 <leonidbarsdev1993@gmail.com>
2022-08-16 15:52:38 +0300

    create user repository and security layer

---
+++ ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.isdmoldova.shipmentcontrolbackend.repository;
+
+import com.isdmoldova.shipmentcontrolbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+}
