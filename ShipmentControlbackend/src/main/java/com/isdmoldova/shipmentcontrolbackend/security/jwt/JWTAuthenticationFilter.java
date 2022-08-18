9fb640d3a75be73e42d3cee35008419127b3d7e7
devLeon93 <leonidbarsdev1993@gmail.com>
2022-08-18 09:06:45 +0300

    SC-1: Modicatation SecurityConfig

--- ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/security/jwt/JWTAuthenticationFilter.java
+++ ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/security/jwt/JWTAuthenticationFilter.java
@@ -1,33 +1,30 @@
 package com.isdmoldova.shipmentcontrolbackend.security.jwt;
 import com.isdmoldova.shipmentcontrolbackend.security.CustomUserDetailsService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
-@RequiredArgsConstructor
+
 @Slf4j
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JWTTokenProvider jwtTokenProvider;
-    private final CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     public static final String HEADER_STRING = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
-
-    public static final String CONTENT_TYPE = "application/json";
-
-
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
