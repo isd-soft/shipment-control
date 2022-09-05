198e7e7bd793e679b911602cbc5cedda516781ae
devLeon93 <leonidbarsdev1993@gmail.com>
2022-09-05 15:19:20 +0300

    refactor applicationSpring

--- ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/ShipmentControlbackendApplication.java
+++ ShipmentControlbackend/src/main/java/com/isdmoldova/shipmentcontrolbackend/ShipmentControlbackendApplication.java
@@ -1,10 +1,4 @@
 package com.isdmoldova.shipmentcontrolbackend;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
