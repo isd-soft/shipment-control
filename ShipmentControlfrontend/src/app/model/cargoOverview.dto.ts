83474c6134f572f567e78338a3296bffead3fc32
Ion Hirjeu <celsiusjon@gmail.com>
2022-09-05 15:21:57 +0300

    Merge fix

--- ShipmentControlfrontend/src/app/model/cargoOverview.dto.ts
+++ ShipmentControlfrontend/src/app/model/cargoOverview.dto.ts
@@ -1,13 +1,8 @@
 export interface CargoDTO{
 
-
-<<<<<<< HEAD
-  cargoStatus : string;
-=======
   trackingNumber : string;
   origin : string;
   destination : string;
   cargoStatus : string[];
->>>>>>> origin/main
 
 }
